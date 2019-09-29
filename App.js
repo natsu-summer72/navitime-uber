761f1ac00307d0670b79b23ed28b5584fed28de2
natsu-summer72 <yjikxipad@gmail.com>
2019-09-29 16:13:24 +0900

    create order page

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { API_KEY, AuthDomain, DatabaseURL, ProjectId, StorageBucket, MessagingSenderId, AppId, MeasurementId } from 'react-native-dotenv'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import * as firebase from 'firebase';
+import Order from './components/order';
+
+/*
+var firebaseConfig = {
+  apikey: API_KEY,
+  authDomain: AuthDomain,
+  databaseURL: DatabaseURL,
+  projectId: ProjectId,
+  storageBucket: StorageBucket,
+  messagingSenderId:MessagingSenderId,
+  appId: AppId,
+  measurementId: MeasurementId
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+*/
+
+export default class App extends React.Component {
+  render() {
+    return (
+            <Order/>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
