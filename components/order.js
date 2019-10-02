9d97c134ee4df700970d2370683f816c5ee99a8f
natsu-summer72 <yjikxipad@gmail.com>
2019-10-02 13:54:36 +0900

    change DB to firestore

--- components/order.js
+++ components/order.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage, Button} from 'react-native-elements'
+import { FormLabel, FormInput, FormValidationMessage, Button} from 'react-native-elements';
 
-import * as firebase from 'firebase'
+import firebase from 'firebase';
+import 'firebase/firestore';
+
 
 export default class Order extends React.Component {
     constructor(props) {
@@ -12,8 +14,7 @@ export default class Order extends React.Component {
 
     render() {
         const {shop_name, shop_address, order} = this.state;
-        let rootRef = firebase.database().ref();
-        let orderRef = rootRef.child('orders');
+        let orderRef = firebase.firestore().collection("orders")
 
         let moment = require('moment')
         let datetime = moment().format("YYYY-MM-DD HH:mm:ssZ")
@@ -27,19 +28,13 @@ export default class Order extends React.Component {
                         onChangeText={shop_name => this.setState({shop_name})}
                         value={shop_name}/>
 
-                    <FormLabel>店住所</FormLabel>
-                    <FormInput
-                        onChangeText={shop_address => this.setState({shop_address})}
-                        value={shop_address}/>
-
                     <FormLabel>オーダー</FormLabel>
                     <FormInput
                         onChangeText={order => this.setState({order})}
                         value={order}/>
                 </View>
                 <Button backgroundColor="#00bfff" title="注文送信" style={{flex:1, justifyContent: 'center'}}
-                        onPress = {() => orderRef.push({'shop-name': this.state.shop_name, 'shop-address': this.state.shop_address,
-                            'order': this.state.order, datetime: datetime,'uid': '0'}) }
+                        onPress = {() => orderRef.add({'shop-name': this.state.shop_name, 'order': this.state.order, datetime: datetime,'uid': '0'}) }
                 />
             </View>
         );
