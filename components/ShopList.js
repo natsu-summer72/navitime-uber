c6ccf5f49d67856cdef5647b8ff1f4e98e015f78
natsu-summer72 <yjikxipad@gmail.com>
2019-10-06 14:06:27 +0900

    Change UI of ShopList

--- components/ShopList.js
+++ components/ShopList.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, ScrollView, Text } from 'react-native';
-import { Tile, Header } from 'react-native-elements';
-import {StatusBar} from "react-native-web";
+import { View, ScrollView, Button } from 'react-native';
+import { Card, Header } from 'react-native-elements';
 import firebase from "firebase";
 
 
@@ -30,26 +29,28 @@ export default class ShopList extends React.Component {
         let list = []
         for (let i=0, len=this.state.list.length; i<len; i++){
             list.push(
-                <Tile
-                    key={i}
-                    imageSrc={require('../img/fast_food.jpg')}
+                <Card
                     title={this.state.list[i]}
-                    titleStyle={{fontSize:20}}
-                    contentContainerStyle={{ height: 70 }}
-                    onPress={()=>this.props.navigation.navigate('Order', {shop_name:this.state.list[i]})}
-                />
+                    image={require('../img/fast_food.jpg')}>
+                    <Button
+                        color={'#00BB00'}
+                        onPress={()=>this.props.navigation.navigate('Order', {shop_name:this.state.list[i]})}
+                        title='注文する' />
+                </Card>
+
             )
         }
 
         return (
-            <View>
+            <View style={{backgroundColor:'#EEEEEE'}}>
                 <Header
                     centerComponent={{ text: 'Shop List', style: { color: '#fff' , fontSize: 20} }}
                 />
-                <ScrollView>
+                <ScrollView style={{backgroundColor:'#EEEEEE', marginBottom:80}}>
                     {list}
                 </ScrollView>
             </View>
         );
     }
 }
+
