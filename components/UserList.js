484a4dafe2d1f25577e92120350b31ef67a7b1eb
natsu-summer72 <yjikxipad@gmail.com>
2019-10-25 21:20:45 +0900

    Change UI of UserList

--- components/UserList.js
+++ components/UserList.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import firebase from 'firebase';
 import {Header, Button} from "react-native-elements";
 
 import { logout } from "../config/firebase";
 import { emails, password } from "../config/firebase";
 
+
+
+const user_list = ["神大生1","神大生2","神大生3","神大生4","神大生5","渋谷JK1","渋谷JK2","渋谷JK3","渋谷JK4","渋谷JK5","ナビタイムジャパン"];
 
 export default class UserList extends React.Component {
     constructor(props){
@@ -15,15 +18,23 @@ export default class UserList extends React.Component {
     render() {
         const login_as_X = (id) => {
             logout()
-            firebase.auth().signInWithEmailAndPassword(emails[id], password)
+            firebase.auth().signInWithEmailAndPassword(emails[id], password[id])
                 .then(response => {
                     alert("Login success!");
-                    console.log(response.user.uid);
                     this.props.navigation.navigate('AfterLogin', {user_info: {uid: response.user.uid, email: response.user.email, phoneNumber: response.user.phoneNumber, name: response.user.displayName}})
                 })
                 .catch(error => {
                     alert(error);
                 });
+        };
+
+        let list = [];
+        for (let i=0, len=user_list.length; i<len; i++){
+            list.push(
+                <View style={{padding:5}}>
+                    <Button onPress={() => login_as_X(i)} title={user_list[i]} />
+                </View>
+            )
         }
 
 
@@ -32,12 +43,9 @@ export default class UserList extends React.Component {
                 <Header
                     centerComponent={{text: 'User List', style: {color: '#fff', fontSize: 20}}}
                 />
-                <View style={{padding:5}}>
-                    <Button onPress={() => login_as_X(0)} title={"ユーザー1"} />
-                </View>
-                <View style={{padding:5}}>
-                    <Button onPress={() => login_as_X(1)} title={"ユーザー2"} />
-                </View>
+                <ScrollView style={{backgroundColor:'#EEEEEE', marginBottom:80}}>
+                    {list}
+                </ScrollView>
             </View>
         )
     }
